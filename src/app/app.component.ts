0770ab32349c1e68e5b071a0b56a8be9a23a07ca
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-02-13 21:56:47 +0100

    feat: Configurado app-config.ts y app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatAnchor } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
-
+import { WalletStore } from '@heavy-duty/wallet-adapter';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { computedAsync } from 'ngxtension/computed-async';
+import { ShyftApiService } from './shyft-api.service';
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'dapp-solana-juan-fuente-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `
+    <header class="px-16 pt-24 pb-8">
+      <h1 class="text-center text-5xl mb-4">This is my bank</h1>
+      <div class="flex justify-center">
+        <hd-wallet-multi-button></hd-wallet-multi-button>
+      </div>
+    </header>
+    <main></main>
+  `,
 })
-export class AppComponent {
-  title = 'dapp-solana-juan-fuente';
-}
+export class AppComponent { }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -3,4 +3,4 @@ import { Buffer } from "buffer";
 
 (window as any).global = window;
 (window as any).global.Buffer = Buffer;
-(window as any).process = { env: {} };  
+(window as any).process = { env: {} };   
