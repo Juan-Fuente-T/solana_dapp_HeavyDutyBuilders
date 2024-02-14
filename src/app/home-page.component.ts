3b228c569a1659107667fdffecefd5e1ae835792
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-02-14 23:31:14 +0100

    Feat: Creacion de footer.component

--- src/app/home-page.component.ts
+++ src/app/home-page.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { FeaturesSectionComponent } from './features-section.component';
+import { FooterComponent } from './footer.component';
 import { HeroSectionComponent } from './hero-section.component';
-
 @Component({
   selector: 'dapp-solana-juan-fuente-home-page',
   template: `
       <dapp-solana-juan-fuente-hero-section></dapp-solana-juan-fuente-hero-section>
       <dapp-solana-juan-fuente-features-section></dapp-solana-juan-fuente-features-section>
+      <dapp-solana-juan-fuente-footer> </dapp-solana-juan-fuente-footer>
 `,
-  imports: [HeroSectionComponent, FeaturesSectionComponent],
+  imports: [HeroSectionComponent, FeaturesSectionComponent, FooterComponent],
   standalone: true
 })
 export class HomePageComponent { }
