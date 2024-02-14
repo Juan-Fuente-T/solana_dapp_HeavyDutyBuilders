0d8570c64deaaf1116052271dbc44c6ecea0136c
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-02-14 02:19:21 +0100

    Feat: Añadido shyft-api.service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { MatAnchor } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
@@ -13,11 +12,25 @@ import { ShyftApiService } from './shyft-api.service';
   template: `
     <header class="px-16 pt-24 pb-8">
       <h1 class="text-center text-5xl mb-4">This is my bank</h1>
-      <div class="flex justify-center">
+      <div class="flex justify-center mb-4">
         <hd-wallet-multi-button></hd-wallet-multi-button>
       </div>
+      @if(account()){
+        <div class="absolute top-4 left-4 justify-center items-center gap-2">
+        <img [src] = "account()?.info?.image" class="w-8 h-8"/>
+        <p class= "text-2xl font-bold">{{ account()?.balance }}</p>
+        </div>
+      }
     </header>
     <main></main>
   `,
 })
-export class AppComponent { }
+export class AppComponent {
+  private readonly _shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
+
+  private readonly account = computedAsync(() => this._shyftApiService.getAccount(this._publicKey()?.toBase58()),
+    { requireSync: true },
+  );
+} 
---
+++ src/app/shyft-api.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { map, of } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class ShyftApiService {
+  private readonly _httpClient = inject(HttpClient);
+  private readonly _headers = { 'x-api-key': 'U6FB9bi1t3_DiqTv' }
+  private readonly _mint = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
+
+  getAccount(publicKey: string | undefined | null) {
+    if (!publicKey) {
+      return of(null);
+    }
+    const url = new URL('https://rpc.shyft.to?api_key=U6FB9bi1t3_DiqTv');
+
+    url.searchParams.set('network', 'mainnet-beta');
+    url.searchParams.set('wallet', publicKey);
+    url.searchParams.set('token', this._mint);
+
+    return this._httpClient.get<{
+      result: { balance: number; info: { image: string } };
+    }>(url.toString(), { headers: this._headers }
+    )
+      .pipe(map((result) => result));
+  }
+}
