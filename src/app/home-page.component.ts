b34ecf90d4415b5175f3b21af73a8db62c4b1ac3
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-02-14 12:27:47 +0100

    Feat: Creado el home-page.component

---
+++ src/app/home-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dapp-solana-juan-fuente-home-page',
+  template: `
+    <section>
+      <h2 class="text-center text-3xl">Home Page></h2>
+      <p class="text-center">Welcome to the Juan Fuente DApp!</p>
+    </section>
+`,
+  standalone: true,
+})
+export class HomePageComponent { }
