217a3424ed7de0c547d621aa84e6015a8a4f41b9
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-02-18 12:28:33 +0100

    Feat: Añade llamada a la api de Shyft para obtener datos del historico de transacciones

--- src/app/shyft-api.service.ts
+++ src/app/shyft-api.service.ts
@@ -1,87 +1,63 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { forkJoin, map, of } from 'rxjs';
+import { map, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ShyftApiService {
   private readonly _httpClient = inject(HttpClient);
-  private readonly _headers = { 'x-api-key': 'U6FB9bi1t3_DiqTv' }
+  private readonly _header = { 'x-api-key': 'U6FB9bi1t3_DiqTv' }
   //private readonly _mint = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
-  private readonly mint = '7EYnhQoR9YM3N7UoaKRoA44Uy8JeaZV3qyouov87awMs';
+  //private readonly mint = '7EYnhQoR9YM3N7UoaKRoA44Uy8JeaZV3qyouov87awMs';
 
-  getAccount(publicKey: string | undefined | null, mint: string) {
+  private readonly _mint = '7EYnhQoR9YM3N7UoaKRoA44Uy8JeaZV3qyouov87awMs';
+
+  getAccount(publicKey: string | undefined | null) {
     if (!publicKey) {
       return of(null);
     }
-    const baseUrl = 'https://api.shyft.to/sol/v1/wallet';
-    const params: { [key: string]: string } = {
-      network: 'mainnet-beta',
-      wallet: publicKey,
-      token: mint
-    };
 
-    const tokenBalanceUrl = new URL(`${baseUrl}/token_balance`);
-    const transactionHistoryUrl = new URL(`${baseUrl}/transaction_history`);
-    const collectionsUrl = new URL(`${baseUrl}/collections`);
+    const url = new URL('https://api.shyft.to/sol/v1/wallet/token_balance');
 
-    Object.keys(params).forEach(key => {
-      tokenBalanceUrl.searchParams.set(key, params[key]);
-      transactionHistoryUrl.searchParams.set(key, params[key]);
-      collectionsUrl.searchParams.set(key, params[key]);
-    });
+    url.searchParams.set('network', 'mainnet-beta');
+    url.searchParams.set('wallet', publicKey);
+    url.searchParams.set('token', this._mint);
 
-    const tokenBalanceRequest = this._httpClient.get<{
-      result: { balance: number; info: { image: string } }
-    }>(tokenBalanceUrl.toString(), { headers: this._headers }
-    )
+    return this._httpClient
+      .get<{
+        result: { balance: number; info: { image: string } };
+      }>(url.toString(), { headers: this._header })
+      .pipe(map((response) => response.result));
+  }
 
-    const transactionHistoryRequest = this._httpClient.get<{
-      success: boolean;
-      message: string;
-      result: {
-        blockTime: number;
-        transaction: {
-          message: {
-            accountKeys: { pubkey: string }[];
-            instructions: {
-              parsed: {
-                info: { destination: string; amount: number };
-              }[];
-            }[];
+  getTransactions(publicKey: string | undefined | null) {
+    if (!publicKey) {
+      return of(null);
+    }
+
+    const url = new URL('https://api.shyft.to/sol/v1/transaction/history');
+
+    url.searchParams.set('network', 'mainnet-beta');
+    url.searchParams.set('account', publicKey);
+    url.searchParams.set('tx-num', '5');
+
+    return this._httpClient
+      .get<{
+        result: {
+          type: string;
+          timestamp: string;
+          actions: {
+            info: {
+              receiver: string;
+              amount: number;
+            };
           };
-        };
-      }[];
-    }>(transactionHistoryUrl.toString(), { headers: this._headers });
-
-    const collectionsRequest = this._httpClient.get<{
-      success: boolean;
-      message: string;
-      result: {
-        collections: {
-          name: string;
-          nft_count: number;
-          nfts: {
-            name: string;
-            mint: string;
-            metadata_uri: string;
-          }[];
         }[];
-      };
-    }>(collectionsUrl.toString(), { headers: this._headers });
-
-    //const transactionHistoryRequest = this._httpClient.get<any>(transactionHistoryUrl.toString(), { headers: this._headers });
-    //const collectionsRequest = this._httpClient.get<any>(collectionsUrl.toString(), { headers: this._headers });
-
-    return forkJoin([tokenBalanceRequest, transactionHistoryRequest, collectionsRequest])
-      .pipe(
-        map(([tokenBalanceResponse, transactionHistoryResponse, collectionsResponse]) => ({
-          tokenBalance: tokenBalanceResponse,
-          transactionHistory: transactionHistoryResponse,
-          collections: collectionsResponse
-        }))
-      )
+      }>(url.toString(), { headers: this._header })
+      .pipe(map((response) => response.result));
   }
 }
+
+
 /*
 shyftApiService.getAccount(publicKey, mint).subscribe(accountData => {
   console.log(accountData); // Aquí tendrás el resultado de la petición
